68b9d154498daf3a70c843a7f84925da76eba4ab
André Dourado <andredourado2008@gmail.com>
2026-09-20 14:40:29 -0300

    update

--- Módulo D/Exercicio-horario-do-dia/script.js
+++ Módulo D/Exercicio-horario-do-dia/script.js
@@ -1,7 +1,20 @@
-function caregar(){
-    var msg = window.document.getElementById('mmsg')
+function carregar(){
+    var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `agora são ${hora} horas` 
+    if (hora >= 0 && hora < 12) {
+        //bom dia 
+        img.src='fotos/foto-entardecer-250.png'
+        document.body.style.background= '#FEB948'
+    } else if (condition) {
+        //boa tarde
+        img.src='fotos/foto-estrada-250.png'
+        document.body.style.background= '#FEB948'
+    }else {
+        //boa noite
+        img.src='fotos/foto-lua-250.png'
+         document.body.style.background= '#FEB948'
+    }
 }
